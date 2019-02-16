f6e5d17e197da2bc0e58c7879d3432ef9f5c7719
MrTallon <352420160@qq.com>
2019-02-17 02:01:03 +0800

    tallon init core

--- tallon/core/Test.py
+++ tallon/core/Test.py
@@ -3,5 +3,5 @@
 # @Author  : YangBo
 # @File    : Test.py
 
-print "Hello World"
-
+# 一行代码的99乘法表
+print('\n'.join([' '.join(['%s*%s=%-2s' % (y, x, x*y) for y in range(1, x+1)]) for x in range(1, 10)]))
