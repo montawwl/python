c032dfc9c7184baa6fc01b49939a8448a18e8577
MrTallon <352420160@qq.com>
2019-06-12 23:08:58 +0800

    long long ago

--- tallon/core/Test.py
+++ tallon/core/Test.py
@@ -5,3 +5,6 @@
 
 # 一行代码的99乘法表
 print('\n'.join([' '.join(['%s*%s=%-2s' % (y, x, x*y) for y in range(1, x+1)]) for x in range(1, 10)]))
+
+# 一行代码实现排序
+quick_sort = lambda array: array if len(array) <= 1 else quick_sort([item for item in array[1:] if item <= array[0]]) + [array[0]] + quick_sort([item for item in array[1:] if item > array[0]])
